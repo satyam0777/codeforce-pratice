4236ee6e7155d7369812c09bfd8ce15167a76409
satyam0777 <officialsatyam0777@gmail.com>
2024-10-17 09:40:44 +0530

    codeforce loops questions

---
+++ sheet-2-CF/loops/cf-4.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+   while(1){
+    int n;
+    cin>>n;
+    if(n==1999){
+        cout<<"Correct"<<endl;
+        break;
+    }else{
+        cout<<"Wrong"<<endl;
+    }
+   }
+ return 0;
+
+    }
+    
+    
