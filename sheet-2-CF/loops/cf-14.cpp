ce2ecaf61eeffdef828caa1fa4fa08a99e944e08
satyam0777 <officialsatyam0777@gmail.com>
2024-10-22 10:02:53 +0530

    numbers histogram

---
+++ sheet-2-CF/loops/cf-14.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char s;
+    int N;
+    cin>>s>>N;
+    while(N--){
+        int x;
+        cin>>x;
+        for(int i=0 ; i<x;i++){
+            cout<<s;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
